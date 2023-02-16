e20302589ef6d1cfe5947d6a560f21c65f1fedbf
Michael alive Lipinski <alkyalive@gmail.com>
2023-02-16 18:59:54 +0400

    Bearer auth

--- src/gateway/client_apollo.ts
+++ src/gateway/client_apollo.ts
@@ -17,8 +17,7 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      // authorization: token ? `Bearer ${token}` : "",
-      "auth-token": token ? token : "",
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
