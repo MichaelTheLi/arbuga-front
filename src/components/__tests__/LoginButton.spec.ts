a937e9b6af32777ea53f66a75f64311c9720e7aa
Michael alive Lipinski <alkyalive@gmail.com>
2023-03-05 16:50:11 +0400

    Login button tests

--- src/components/__tests__/LoginButton.spec.ts
+++ src/components/__tests__/LoginButton.spec.ts
@@ -1,31 +1,59 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import { installQuasar } from "@quasar/quasar-app-extension-testing-unit-vitest";
 import { mount } from "@vue/test-utils";
-import LoginButtonInner from "../Account/LoginButtonInner.vue";
-import { QDialog } from "quasar";
 import LoginButton from "../Account/LoginButton.vue";
 
 installQuasar();
 
+const executeMock = vi.fn();
+vi.mock("@/gateway/gateway", () => {
+  return {
+    useLoginUser: () => ({ execute: executeMock }),
+  };
+});
+
 describe("LoginButton", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
   const mountComponent = () => {
-    return mount(LoginButton, {
-      stubs: {
-        LoginButtonInner: {
-          template:
-            "<span data-testid='test-login-form' @testlogin='$emit(\"login\")' @testsignon='$emit(\"signon\")' />",
+    const wrapper = mount(LoginButton, {
+      global: {
+        stubs: {
+          LoginButtonInner: {
+            template:
+              '<span data-testid=\'test-login-inner\' @testlogin=\'$emit("login", {"test":2})\' @testsignon=\'$emit("signon", {"test":3})\' />',
+          },
         },
       },
     });
+    return { wrapper };
   };
 
-  it.todo("renders initial properly. TODO Apollo client issues", () => {
-    const wrapper = mountComponent();
+  it("renders initial properly", () => {
+    const { wrapper } = mountComponent();
 
     expect(
-      wrapper.get('[data-testid="login-button"]').isVisible()
+      wrapper.get('[data-testid="test-login-inner"]').isVisible()
     ).toBeTruthy();
+  });
 
-    expect(wrapper.getComponent(QDialog).isVisible()).toBeFalsy();
+  it("login event routed", async () => {
+    const { wrapper } = mountComponent();
+
+    await wrapper.get('[data-testid="test-login-inner"]').trigger("testlogin");
+
+    expect(executeMock).toBeCalledTimes(1);
+    expect(executeMock).toBeCalledWith({ test: 2 });
+  });
+
+  it("signon event routed", async () => {
+    const { wrapper } = mountComponent();
+
+    await wrapper.get('[data-testid="test-login-inner"]').trigger("testsignon");
+
+    expect(executeMock).toBeCalledTimes(1);
+    expect(executeMock).toBeCalledWith({ test: 3 });
   });
 });
