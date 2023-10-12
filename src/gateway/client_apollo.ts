74b36e4d35402058fcd91a41532409e20461df56
Michael alive Lipinski <alkyalive@gmail.com>
2023-10-12 21:51:23 +0400

    Retry on network 401/403 errors

--- src/gateway/client_apollo.ts
+++ src/gateway/client_apollo.ts
@@ -1,33 +1,66 @@
 import {
   ApolloClient,
   createHttpLink,
+  from,
   InMemoryCache,
 } from "@apollo/client/core";
 import typeDefs from "@/gateway/schema";
 import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
+import { RetryLink } from "@apollo/client/link/retry";
 
 const httpLink = createHttpLink({ uri: import.meta.env.VITE_BACKEND_URL });
 
 const authLink = setContext((request, { headers }) => {
-  let additionalHeaders = {};
+  return new Promise((success) => {
+    let additionalHeaders = {};
 
-  if (request.operationName != "userLogin") {
-    const token = localStorage.getItem("token");
+    if (request.operationName != "userLogin") {
+      const token = localStorage.getItem("token");
 
-    additionalHeaders = {
-      authorization: token ? `Bearer ${token}` : "",
-    };
-  }
+      additionalHeaders = {
+        authorization: token ? `Bearer ${token}` : "",
+      };
+    }
 
-  return {
-    headers: {
-      ...headers,
-      ...additionalHeaders,
-    },
-  };
+    success({
+      headers: {
+        ...headers,
+        ...additionalHeaders,
+      },
+    });
+  });
 });
 
-const link = authLink.concat(httpLink);
+const graphqlErrorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  }
+});
+
+const networkErrorLink = onError(({ networkError }) => {
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+
+    if (
+      "statusCode" in networkError &&
+      (networkError.statusCode === 401 || networkError.statusCode === 403)
+    ) {
+      localStorage.removeItem("token");
+    }
+  }
+});
+
+const retryLink = new RetryLink({
+  delay: {
+    initial: 1000,
+  },
+});
+
 export const cache = new InMemoryCache({
   possibleTypes: {
     Animal: ["Fish", "Plant"],
@@ -37,7 +70,13 @@ export const cache = new InMemoryCache({
 const resolvers = {};
 
 export const apolloClient = new ApolloClient({
-  link,
+  link: from([
+    retryLink,
+    graphqlErrorLink,
+    networkErrorLink,
+    authLink,
+    httpLink,
+  ]),
   cache,
   typeDefs,
   resolvers,
